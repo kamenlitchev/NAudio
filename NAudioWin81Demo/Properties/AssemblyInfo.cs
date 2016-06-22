3af4d8561b6b0ec2b0c37198f2fffcf101f54676
Kamen Lichev <kamenlitchev@users.noreply.github.com>
2016-06-22 21:01:26 +0300

    Retarget to Win81; Support Windows Phone 81

--- NAudioWin81Demo/Properties/AssemblyInfo.cs
+++ NAudioWin81Demo/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NAudio.Win8")]
+[assembly: AssemblyTitle("NAudioWin81Demo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NAudio.Win8")]
-[assembly: AssemblyCopyright("Copyright © Mark Heath 2013")]
+[assembly: AssemblyProduct("NAudioWin81Demo")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.0.15")]
-[assembly: AssemblyFileVersion("1.7.0.15")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
